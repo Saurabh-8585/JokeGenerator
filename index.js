343fd2c09ef4be9a95a10d3f200e6b42f008ec4e
Saurabh Khatmode <khatmodesaurabh7@gmail.com>
2022-09-12 15:36:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const englishJokes = [
+    "What do kids play when their mom is using the phone ? Bored games.",
+    "What do you call an ant who fights crime ? A vigilANTe!",
+    "Why are snails slow ? Because they’re carrying a house on their back.",
+    "What’s the smartest insect ? A spelling bee!",
+    "What does a storm cloud wear under his raincoat ? Thunderwear.",
+    "What is fast, loud and crunchy ? A rocket chip.",
+    "How does the ocean say hi ? It waves!",
+    "What do you call a couple of chimpanzees sharing an Amazon account ? PRIME - mates.",
+    "Why did the teddy bear say no to dessert ? Because she was stuffed.",
+    " Why did the soccer player take so long to eat dinner ? Because he thought he couldn’t use his hands.",
+    "Name the kind of tree you can hold in your hand ? A palm tree!",
+    "What do birds give out on Halloween ? Tweets.",
+    "What has ears but cannot hear ? A cornfield.",
+    "What’s a cat’s favorite dessert ? A bowl full of mice - cream.",
+    "Where did the music teacher leave her keys ? In the piano!",
+    "What did the policeman say to his hungry stomach ? “Freeze.You’re under a vest.”",
+    "What did the left eye say to the right eye ? Between us, something smells!",
+    "What do you call a guy who’s really loud ? Mike.",
+    "Why do birds fly south in the winter ? It’s faster than walking!",
+    "What did the lava say to his girlfriend ? “I lava you!”",
+    "Why did the student eat his homework ? Because the teacher told him it was a piece of cake.",
+    "What did Yoda say when he saw himself in 4k ? HDMI.",
+    "Which superhero hits home runs ? Batman!",
+    "What’s Thanos’ favorite app on his phone ? Snapchat.",
+    "Sandy’s mum has four kids; North, West, East.What is the name of the fourth child ? Sandy, obviously!",
+    "What is a room with no walls ? A mushroom.",
+    "Why did the blue jay get in trouble at school ? For tweeting on a test!",
+    "What social event do spiders love to attend ? Webbings.",
+    "What did one pickle say to the other ? Dill with it.short jokes about coconut on vacation",
+    "What is brown, hairy and wears sunglasses ? A coconut on vacation.",
+    "Why is a football stadium always cold ? It has lots of fans!",
+    " What did one math book say to the other ? “I’ve got so many problems.”",
+    "What did the Dalmatian say after lunch ? That hit the spot!",
+    "What do you call two bananas on the floor ? Slippers.",
+    "Why did the chicken cross the playground ? To get to the other slide.",
+    "Why do ducks have feathers on their tails ? To cover their buttquacks.",
+    "How does a vampire start a letter ? “Tomb it may concern…”",
+    "A plane crashed in the jungle and every single person died.Who survived ? Married couples.",
+    "What kind of math do birds love ? Owl - gebra!",
+    "Why can’t you ever tell a joke around glass ? It could crack up.",
+    "What do you call a Star Wars droid that takes the long way around ? R2 detour.",
+    "How do you stop an astronaut’s baby from crying ? You rocket.",
+    "Why did the scarecrow win a Nobel prize ? Because she was outstanding in her field.",
+    "How do you know when a bike is thinking ? You can see their wheels turning.",
+    "Why was 6 afraid of 7 ? Because 7, 8, 9.",
+    "What goes up and down but doesn’t move ? The staircase.",
+    "What kind of shoes do frogs love ? Open - toad!",
+    "How did the baby tell his mom he had a wet diaper ? He sent her a pee - mail.",
+    "What is a witch’s favorite subject in school ? Spelling.",
+    "What’s brown and sticky ? A stick"
+];
+
+
+const hindiJokes = [
+    "घर जमाई: आज से मैं रोटी नहीं चावल खाऊंगा इलेक्ट्रॉनिक्स और एक्सेसरीज पर बंपर छूट, महज 99 रुपये से शुरूसास - ऐसा क्यों ? घर जमाई - मोहल्ले वालों के ताने सुनकर थक गया हूं रोज कहते हैं कि ससुराल वालों की रोटी तोड़ता हूं",
+
+    "पत्नी - आज मैंने तुम्हारे लिए एक स्पेशल डिश बनाई है,खाते ही गर्मी गायब  पति - ऐसा क्या बना दिया तुमने यार ? पत्नी - नवरत्न तेल के पकौड़े!",
+
+    "पिंकी तेजी से स्कूटी पे जा रही थी, जैसे ही रेड लाइट क्रॉस की पुलिसवाला - मैडम गाड़ी साइड लगाओ पिंकी - पहली बार है माफ कर दो ना पुलिसवाला - आपको रेड लाइट नहीं दिखी थी क्या ? पिंकी - लाइट तो दिखी थी, लेकिन आप कहीं नहीं दिखे, जाने कहां छिपे थे",
+
+    "टीचर - एक टोकरी में 10 आम थे 3 सड़ गए तो कितने बचे ? बंटू - 10  टीचर - अरे मूर्ख 10 कैसे बचेंगे ? बंटू - सड़े हुए आम कहां जाएंगे, सड़ने से केले थोड़ी बन जाएंगे",
+
+    "लड़की - भैया मुझे चप्पल लेनी है दुकानदार - ये देखो बहन जी नया डिजाइन दुकानदार - बहन जी अब तो सारी चप्पल दिखा दी   दुकानदार - रहम करो बहन जी लड़की - और दिखाओ ये नहीं और दिखाओअब तो कोई नहीं बची लड़की - अरे भैया वो एक और डिब्बा रखा है ना वो मेरा लंच का डिब्बा है"
+];
+const marathiJokeS = [
+    "मोलकरीण: बाईसाहेब तुमच्या मुलाने मच्छर खाल्ले,बाईसाहेब: माझ तोंड काय बघतेस डॉक्टरला बोलाव,मोलकरीण: आता घाबरण्याचे कारण नाही बाईसाहेब.मी त्याला All Out पाजले आहे.",
+
+    "पोलीसः सिग्नल दिसत नाही का ? चल पावती फाड चालकः तिकडं मल्ल्या, निरव - ललित मोदी हजारो करोड पोलीसः ते फेसबुकवर टाक, इथे गप पावती फाड",
+
+    "सासू: “कित्ती वेळा सांगितलंय बाहेर जातांना टिकली लावत जा” सूनबाई: “अहो सासूबाई, जीन्सवर कुणी टिकली नाही लावत”सासू: “अगं जीन्सवर नाही, कपाळावर लाव, भवाने!”",
+
+    "तुम्ही जर जग बदलू इच्छित असाल, तर अविवाहित असतांना बदला.लग्नानंतर तुम्ही टीव्ही चॅनेल पण बदलू शकत नाही",
+
+    "गणपतीला दोन बायका असतात, रिद्धी आणि सिद्धी सामान्य माणसाला एकच बायको असते, ती पण जिद्दी",
+
+    " शिक्षक: उद्या गृहपाठ नाही करून आणलास तर कोंबडा बनवेन पप्पू: ओके सर, पण जरा झणझणीत बनवा, मी रॉयल स्टॅगचा खंबा घेऊन येतो",
+
+    "नितेश पहिल्यांदाच मुलगी बघायला गेला मुलीचा बाप : बेटा, दारू पितोस का ?नितेश : ते नंतर, आधी पोहे, चहा आणि मुलगी बघणे तर होऊ द्या, मग बसू!",
+
+    "मुलगा बाबांच्या लग्नाची सिडी बघत असतो.मुलगा: बाबा मला तुमच्या लग्नासारख्या माझ्या आयटम गर्ल्स नाचवायच्या आहेत..आई: हरामखोर, त्या तुझ्या मावश्या आहेत",
+
+    "गुरुजी - काय समजले नसेल तर विचारा..बंड्या - गुरुजी फळा पुसल्यावर फळ्यावरील अक्षरे कुठे जातात गुरुजी डोकं आपटून मेले ",
+
+    "तिचा फोन आला, खूप अकडत अडकत ती म्हणाली,विसरुन जा मलामी म्हणालो,आधी नाव तरी सांग कोण आहेस तू ? "
+];
+
+
+const InputEnglish = document.getElementById("english");
+const InputHindi = document.getElementById("hindi");
+const InputMarathi = document.getElementById("marathi");
+
+const generateBtn = document.getElementById("generate");
+const copyBtn = document.getElementById("copy");
+const displayjoke = document.getElementById("displayJoke");
+
+
+
+const generateJoke = () => {
+    if (InputEnglish.checked) {
+        jokeFunction(englishJokes)
+    }
+    if (InputHindi.checked) {
+        jokeFunction(hindiJokes)
+    }
+    if (InputMarathi.checked) {
+        jokeFunction(marathiJokeS)
+    }
+}
+
+const jokeFunction = (arr) => {
+    let randomNumber = Math.floor(Math.random() * (arr.length))
+    document.getElementById("displayJoke").innerText = arr[randomNumber]
+}
+
+generateBtn.addEventListener("click", function () {
+    copyBtn.innerText = "Copy"
+    generateJoke()
+})
+copyBtn.addEventListener("click", function () {
+    displayjoke.select();
+    displayjoke.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(displayjoke.value);
+    setTimeout(() => {
+        copyBtn.innerText = 'Copied'
+    }, 200)
+    clearTimeout();
+
+})
+
